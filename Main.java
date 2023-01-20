ca3daf784147fe462d6a34c15265f09d129a721e
김도영 <112838087+CreatorDodo@users.noreply.github.com>
2023-01-20 03:48:42 +0000

    230120

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+
+    double a = 3;
+    double b = 2;
+    
+    if(a > b) {
+      System.out.println(a);
+      
+    } else if(a < b) {
+                System.out.println(b);
+    } else {
+      System.out.println(a);
+    }
+  }
+}
